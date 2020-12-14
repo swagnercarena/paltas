Fix sign for minimum size cut...

manada/Sources/cosmos.py
@@ -81,7 +81,7 @@ class COSMOSCatalog(GalaxyCatalog):
         if minimum_size_in_pixels is not None:
             min_size = np.minimum(self.catalog['size_x'],
                                   self.catalog['size_y'])
-            is_ok &= min_size < minimum_size_in_pixels
+            is_ok &= min_size >= minimum_size_in_pixels
         return np.random.choice(np.where(is_ok)[0],
                                 size=n_galaxies,
                                 replace=True)