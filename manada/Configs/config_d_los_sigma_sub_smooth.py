bcd475dcaee0e01fb4eefac8d220b174dd32ff24
Jelle Aalbers <jelleaalbers@gmail.com>
2021-03-25 13:02:02 -0700

    Move Configs dir inside package, add smooth config

---
+++ manada/Configs/config_d_los_sigma_sub_smooth.py
@@ -0,0 +1,4 @@
+from manada.Sources.cosmos import COSMOSSersicCatalog
+from manada.Configs.config_d_los_sigma_sub import *
+
+config_dict['source']['class'] = COSMOSSersicCatalog
--- manada/__init__.py
+++ manada/__init__.py
@@ -2,6 +2,8 @@ __author__ = 'Sebastian Wagner-Carena'
 __email__ = 'sebaswagner@outlook.com'
 __version__ = '0.0.1'
 
+from . import Configs
+from . import Sampling
 from . import Sources
 from . import Substructure
 from . import Utils
