e8af1b329a81b0ef7d8f42d5867709b6eafee2cf
Jelle Aalbers <jelleaalbers@gmail.com>
2021-05-05 17:05:04 -0700

    Use COSMOS's, not manada's pixel width for scaling

--- manada/Configs/config_random_sersic.py
+++ manada/Configs/config_random_sersic.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from manada.Sources.sersic import SingleSersicSource
+from manada.Sources.cosmos import HUBBLE_ACS_PIXEL_WIDTH
 from manada.Configs.config_d_los_sigma_sub import *
 
 from scipy import stats
@@ -12,9 +13,10 @@ config_dict['source']['class'] = SingleSersicSource
 config_dict['source']['parameters'] = dict(
 	z_source=config_dict['source']['parameters']['z_source'],
 	# Fitted to COSMOS Sersicfit results
-	amp=lambda: stats.lognorm(s=0.895, scale=0.0410).rvs() / 0.08**2,
-	# z_scaling to 1.5 is applied before fit
-	R_sersic=lambda : 0.08 * stats.lognorm(s=0.554, scale=17.1).rvs(),
+	# Note the pixel scaling uses the COSMOS pixel width , not Manada's
+	amp=lambda: stats.lognorm(s=0.895, scale=0.0410).rvs() / HUBBLE_ACS_PIXEL_WIDTH**2,
+	# z_scaling to 1.5 is applied before fit.
+	R_sersic=lambda : HUBBLE_ACS_PIXEL_WIDTH * stats.lognorm(s=0.554, scale=17.1).rvs(),
 	# NOT fitted to COSMOS.
 	# COSMOS has a big mass at the maximum n=6, probably indicating poor fits / very spread out galaxies?
 	n_sersic=1.,
