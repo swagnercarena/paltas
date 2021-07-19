da5f0c909016b72f11e19c5e2b46de0e713545c2
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2021-07-19 11:22:53 -0400

    changed training config to include log parameters

--- manada/Analysis/AnalysisConfigs/train_config.py
+++ manada/Analysis/AnalysisConfigs/train_config.py
@@ -8,11 +8,12 @@ img_size = (64,64,1)
 # A random seed to us
 random_seed = 2
 # The list of learning parameters to use
-learning_params = ['subhalo_parameters_sigma_sub',
-	'main_deflector_parameters_theta_E','main_deflector_parameters_gamma1',
-	'main_deflector_parameters_gamma2','main_deflector_parameters_gamma',
-	'main_deflector_parameters_e1','main_deflector_parameters_e2',
-	'main_deflector_parameters_center_x','main_deflector_parameters_center_y',
+learning_params = ['main_deflector_parameters_theta_E',
+	'main_deflector_parameters_gamma1','main_deflector_parameters_gamma2',
+	'main_deflector_parameters_gamma','main_deflector_parameters_e1',
+	'main_deflector_parameters_e2','main_deflector_parameters_center_x',
+	'main_deflector_parameters_center_y']
+log_learning_params = ['subhalo_parameters_sigma_sub',
 	'los_parameters_delta_los']
 # Which parameters to consider flipping
 flip_pairs = None
@@ -39,7 +40,7 @@ metadata_paths_train = [
 metadata_path_val = os.path.join(npy_folder_val,'metadata.csv')
 # The path to the csv file to read from / write to for normalization
 # of learning parameters.
-input_norm_path = npy_folders_train[0] + 'norms.csv'
+input_norm_path = npy_folders_train[0] + 'log_norms.csv'
 # The detector kwargs to use for on-the-fly noise generation
 kwargs_detector = {'pixel_scale':0.08,'ccd_gain':2.5,'read_noise':4.0,
 	'magnitude_zero_point':25.9463,'exposure_time':5400.0,'sky_brightness':22,
