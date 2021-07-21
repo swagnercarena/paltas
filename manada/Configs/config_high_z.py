a934c065bf7ead7ba317d1a24e1a632e4e37b826
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2021-07-21 00:05:55 -0400

    added a config to make a high z training set for comparison

---
+++ manada/Configs/config_high_z.py
@@ -0,0 +1,5 @@
+from manada.Configs.config_d_los_sigma_sub import *
+from scipy.stats import norm
+
+config_dict['main_deflector']['parameters']['z_lens'] = 1.0
+config_dict['source']['parameters']['z_source'] = 3.0
