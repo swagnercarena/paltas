8d2d0af05180b0ed805f92d01d3f1cecc645e2b0
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2021-08-11 13:33:13 -0600

    put the test slope more in the region of the detectable

--- manada/Configs/config_as_test.py
+++ manada/Configs/config_as_test.py
@@ -1,7 +1,7 @@
 from manada.Configs.config_d_los_sigma_sub import *
 from scipy.stats import norm
 
-config_dict['subhalo']['parameters']['shmf_plaw_index'] = norm(loc=-1.83,
+config_dict['subhalo']['parameters']['shmf_plaw_index'] = norm(loc=-1.8,
 	scale=0.001).rvs
 config_dict['subhalo']['parameters']['sigma_sub'] = norm(loc=1.3e-1,
 	scale=1e-2).rvs
