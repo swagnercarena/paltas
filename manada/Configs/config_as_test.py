e98d8b32077e10c03e38731ae923c97aac8d6f16
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2021-08-16 15:09:35 -0700

    test distributions changed to be less tight

--- manada/Configs/config_as_test.py
+++ manada/Configs/config_as_test.py
@@ -1,9 +1,7 @@
 from manada.Configs.config_d_los_sigma_sub import *
-from scipy.stats import norm
+from scipy.stats import norm, lognorm
 
 config_dict['subhalo']['parameters']['shmf_plaw_index'] = norm(loc=-1.8,
-	scale=0.001).rvs
-config_dict['subhalo']['parameters']['sigma_sub'] = norm(loc=1.3e-1,
-	scale=1e-2).rvs
-config_dict['los']['parameters']['delta_los'] = norm(loc=1.2,
-	scale=0.001).rvs
+	scale=0.01).rvs
+config_dict['subhalo']['parameters']['sigma_sub'] = lognorm(scale=3.0e-3,
+	s=0.1).rvs
