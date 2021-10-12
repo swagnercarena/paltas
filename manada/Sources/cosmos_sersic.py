8c3c4f78b7ab9e2977f2c003ed8d0c90d4a27c31
Sydney E <smericks@umich.edu>
2021-10-11 23:11:20 -0700

    Adding COSMOSSersic file

---
+++ manada/Sources/cosmos_sersic.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Generate Lenstronomy inputs for a COSMOS galaxy + a Sersic Ellipse
+
+This module contains a class that combines a COSMOS galaxy and a Sersic light 
+distribution as the two sources for manada.
+"""
+from .cosmos import COSMOSCatalog
+from .sersic import SingleSersicSource
+
+class COSMOSSersic(COSMOSCatalog):
+    
+    required_parameters = ('minimum_size_in_pixels','min_apparent_mag','max_z',
+		'smoothing_sigma','cosmos_folder','random_rotation','min_flux_radius',
+		'output_ab_zeropoint','z_source', 'mag_sersic', 'R_sersic', 'n_sersic', 
+    'e1_sersic', 'e2_sersic', 'center_x_sersic', 'center_y_sersic')
+
+    def draw_source(self):
+      
+        model_list, kwargs_list = COSMOSCatalog.draw_source(self)
+        model_list.append('SERSIC_ELLIPSE')
+
+        #create dict for sersic
+        sersic_kwargs_dict = {}
+        suffix = '_sersic'
+        for param_name in self.__class__.required_parameters:
+          if suffix in param_name:
+            if ( param_name == 'R_sersic' or param_name == 'n_sersic') :
+              sersic_kwargs_dict[param_name] = self.source_parameters[param_name]
+            else :
+              sersic_kwargs_dict[param_name[:-7]] = self.source_parameters[param_name]
+
+        # mag to amp conversion
+        sersic_kwargs_dict.pop('mag')
+        sersic_kwargs_dict['amp'] = SingleSersicSource.mag_to_amplitude(
+          self.source_parameters['mag_sersic'], [sersic_kwargs_dict])
+
+        kwargs_list.append(sersic_kwargs_dict)
+
+        return model_list, kwargs_list
