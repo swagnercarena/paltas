42c25207a42c8187a1a58a4ff677b83df45b2217
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2021-11-03 12:06:49 -0700

    got rid of final test for draw source on the point source

--- test/point_source_tests.py
+++ test/point_source_tests.py
@@ -79,7 +79,7 @@ class SinglePointSourceTests(PointSourceBaseTests):
         point_source_model = PointSource(list_ps_model)
         
         # define PSF class, data class
-        n_pixels = 128
+        n_pixels = 64
         pixel_width = 0.08
         psf_class = PSF(psf_type='NONE')
         data_class = ImageData(**data_configure_simple(numPix=n_pixels,
@@ -115,62 +115,4 @@ class SinglePointSourceTests(PointSourceBaseTests):
 
         # make sure light is in the center of the image
         # 128 x 128 image
-        self.assertTrue( np.sum(im_diff[60:68,60:68]) > 0 )
-
-        # test image positions compared to a sersic w/ high n_sersic
-        # define lens, psf, data classes
-        lens_kwargs[0]['gamma'] = 2.0
-        lens_kwargs[0]['theta_E'] = 0.5
-        psf_class = PSF(psf_type='GAUSSIAN', fwhm=0.1 * pixel_width)
-
-        # set up COSMOS
-        test_cosmo_folder = (os.path.dirname(
-			os.path.abspath(__file__))+'/test_data/cosmos/')
-        # set high n sersic, low R sersic
-        cosmossersic_params = {
-			'smoothing_sigma':0, 'max_z':None, 'minimum_size_in_pixels':None,
-			'min_apparent_mag':None,'cosmos_folder':test_cosmo_folder,
-			'random_rotation':False, 'min_flux_radius':None,
-			'output_ab_zeropoint':25, 'z_source':1.5,
-			'mag_sersic':21, 'R_sersic':0.001, 'n_sersic':6.5, 
-  			'e1_sersic':0, 'e2_sersic':0, 'center_x_sersic':0, 
-			  'center_y_sersic':0}
-        cosmos = COSMOSCatalog(cosmology_parameters='planck18',
-			source_parameters=cosmossersic_params)
-        light_model_list, light_kwargs = cosmos.draw_source(0)
-
-        # PointSource
-        self.point_source_parameters['magnitude'] = 18.75
-        self.point_source_parameters['x_point_source'] = 0.
-        self.point_source_parameters['y_point_source'] = 0.
-        self.c.update_parameters(self.point_source_parameters)
-        list_ps_model, point_source_kwargs = self.c.draw_point_source()
-        point_source_model = PointSource(list_ps_model)
-    
-        # COSMOS + Point Source
-        point_source_model = PointSource(list_ps_model)
-        light_model = LightModel(light_model_list)
-        image_model = ImageModel(data_class=data_class, psf_class=psf_class,
-            lens_model_class=lens_model, source_model_class=light_model,
-            point_source_class=point_source_model)
-        im_cosmos_ps = image_model.image(kwargs_lens=lens_kwargs, 
-            kwargs_source=light_kwargs,kwargs_ps=point_source_kwargs)
-        
-        # COSMOS + Sersic
-        cosmossersic = COSMOSSersic(cosmology_parameters='planck18', 
-            source_parameters=cosmossersic_params)
-        light_model_list, light_kwargs = cosmos.draw_source(0)
-        light_model = LightModel(light_model_list)
-        image_model = ImageModel(data_class=data_class, psf_class=psf_class,
-            lens_model_class=lens_model,source_model_class=light_model)
-        im_cosmossersic = image_model.image(kwargs_lens=lens_kwargs, 
-            kwargs_source=light_kwargs)
-
-        # TODO: see if images are in the same location
-        # maybe ask to see if this is even a good idea??
-
-        # tear down COSMOS
-        os.remove(test_cosmo_folder+'manada_catalog.npy')
-        for i in range(10):
-            os.remove(test_cosmo_folder+'npy_files/img_%d.npy'%(i))
-        os.rmdir(test_cosmo_folder+'npy_files')
+        self.assertTrue( np.sum(im_diff[30:34,30:34]) == flux_image)
