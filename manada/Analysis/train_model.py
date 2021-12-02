95e80cb0240ff4a77b3c7269879f4451d0419dc9
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2021-12-01 18:02:07 -0800

    suspend only saving if validation loss is minimized since we're doing an ensemble of models. This way we can also use the same sources for validation and testing

--- manada/Analysis/train_model.py
+++ manada/Analysis/train_model.py
@@ -223,8 +223,8 @@ def main():
 			update_freq='batch')
 		callbacks.append(tensorboard)
 	# Save the model weights as long as the validation loss is decreasing
-	modelcheckpoint = ModelCheckpoint(model_weights,monitor='val_loss',
-		save_best_only=True,save_freq='epoch')
+	# modelcheckpoint = ModelCheckpoint(model_weights,monitor='val_loss',
+	# 	save_best_only=True,save_freq='epoch')
 	callbacks.append(modelcheckpoint)
 
 	# TODO add validation data.
