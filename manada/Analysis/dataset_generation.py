covariance rotation bug fixed

manada/Analysis/dataset_generation.py
@@ -362,26 +362,36 @@ def rotate_covariance_batch(learning_params,coavriance_batch,rot_angle):
 		return np.array([[np.cos(theta),-np.sin(theta)],
 			[np.sin(theta),np.cos(theta)]])
 
+	# For each possible pair of parameters, conduct the rotation.
 	if 'main_deflector_parameters_center_x' in learning_params:
 		xi = learning_params.index('main_deflector_parameters_center_x')
 		yi = learning_params.index('main_deflector_parameters_center_y')
+		# Ensure the parameters are next to each other. Otherwise this
+		# indexing will fail.
+		assert xi == yi - 1
+		yi+=1
 		rot_mat = rotation_matrix(rot_angle)
-		coavriance_batch[:,[xi,yi]] = np.dot(rot_mat,
-			np.dot(coavriance_batch[:,[xi,yi]],rot_mat.T).T).T
+		coavriance_batch[:,xi:yi,xi:yi] = np.dot(rot_mat,
+			np.dot(coavriance_batch[:,xi:yi,xi:yi],rot_mat.T).T).T
 
 	if 'main_deflector_parameters_e1' in learning_params:
 		xi = learning_params.index('main_deflector_parameters_e1')
 		yi = learning_params.index('main_deflector_parameters_e2')
+		# Ensure the parameters are next to each other.
+		assert xi == yi - 1
+		yi+=1
 		rot_mat = rotation_matrix(2*rot_angle)
-		coavriance_batch[:,[xi,yi]] = np.dot(rot_mat,
-			np.dot(coavriance_batch[:,[xi,yi]],rot_mat.T).T).T
+		coavriance_batch[:,xi:yi,xi:yi] = np.dot(rot_mat,
+			np.dot(coavriance_batch[:,xi:yi,xi:yi],rot_mat.T).T).T
 
 	if 'main_deflector_parameters_gamma1' in learning_params:
 		xi = learning_params.index('main_deflector_parameters_gamma1')
 		yi = learning_params.index('main_deflector_parameters_gamma2')
+		assert xi == yi - 1
+		yi+=1
 		rot_mat = rotation_matrix(2*rot_angle)
-		coavriance_batch[:,[xi,yi]] = np.dot(rot_mat,
-			np.dot(coavriance_batch[:,[xi,yi]],rot_mat.T).T).T
+		coavriance_batch[:,xi:yi,xi:yi] = np.dot(rot_mat,
+			np.dot(coavriance_batch[:,xi:yi,xi:yi],rot_mat.T).T).T
 
 
 def rotate_image_batch(image_batch,learning_params,output,rot_angle):

test/analysis_tests.py
@@ -297,11 +297,11 @@ class DatasetGenerationTests(unittest.TestCase):
 			'main_deflector_parameters_center_y']
 
 		# Generate a bunch of draws from this base covariance matrix
-		cov_mat_gen = np.array([[2.0,0.3],[0.3,1.1]])
+		cov_mat_gen = np.array([[2.0,0.3,0.0],[0.3,1.1,0.0],[0.0,0.0,1.0]])
 		n_samps = int(5e5)
-		y_pred = np.random.multivariate_normal(mean=np.zeros(2),
+		y_pred = np.random.multivariate_normal(mean=np.zeros(3),
 			cov=cov_mat_gen,size=n_samps)
-		cov_mats = np.tile(cov_mat_gen,n_samps).T.reshape(n_samps,2,2)
+		cov_mats = np.tile(cov_mat_gen,n_samps).T.reshape(n_samps,3,3)
 
 		# Rotate the covariance matrix and make sure it squares with the
 		# rotated covariance matrices