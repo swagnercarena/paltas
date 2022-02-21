79bfdb88b9b0e509e16917e1d2ccbd9e1703b87a
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2022-02-21 15:10:19 -0800

    lens models now has length

--- paltas/Substructure/subhalos_catalog.py
+++ paltas/Substructure/subhalos_catalog.py
@@ -355,7 +355,7 @@ class SubhalosCatalog(SubhalosBase):
 				kwargs_new.append({'Rs': rr_host.values[0], 'alpha_Rs': alpha_Rs_host.values[0],  'center_x': 0, 'center_y':0})
 		lens_analytic = LensModel(lens_model_list=profiles, lens_redshift_list=z_lens, z_source=z_source, cosmo=cosmo, multi_plane=False)
 
-		return lens_analytic, kwargs_new
+		return [lens_analytic]*len(kwargs_new), kwargs_new
 
 
 	def draw_subhalos(self):
