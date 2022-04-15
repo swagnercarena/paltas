4dca13ab8d40184adbaa935c38b0b6cc0ef7293c
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2022-04-15 15:23:09 -0400

    more template files

---
+++ paltas/Substructure/subhalos_wdm.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Draw subhalo masses and concentrations for NFW subhalos in a WDM universe
+following a combination of https://arxiv.org/pdf/1909.02573.pdf,
+https://arxiv.org/pdf/2003.01125.pdf, and https://arxiv.org/pdf/1507.01998.pdf.
+
+This module contains the functions needed to turn the parameters of warm dark
+matter subhalo distributions into masses, concentrations, and positions for
+those subhalos.
+"""
