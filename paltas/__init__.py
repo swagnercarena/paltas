838e7ed789610747a8531e70401d024949f0a382
Sydney Erickson <smericks@dhcp-visitor-216-225.slac.stanford.edu>
2022-09-30 13:57:07 -0700

    Fix train_model.py bugs, fix Analysis module import

--- paltas/__init__.py
+++ paltas/__init__.py
@@ -3,7 +3,13 @@ __email__ = 'sebaswagner@outlook.com'
 __version__ = '0.1.1'
 
 # Analysis is not imported by default because it required tensorflow.
-
+try:
+    import tensorflow as tf
+    del tf
+except ImportError:
+    print("paltas.Analysis disabled since tensorflow is missing")
+else:
+    from . import Analysis
 from . import Configs
 from . import Sampling
 from . import Sources
