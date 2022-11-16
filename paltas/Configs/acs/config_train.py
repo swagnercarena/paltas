1f27b65da625132a26db18ed51af8f6aa125561c
Jelle Aalbers <jelleaalbers@gmail.com>
2022-11-16 13:59:25 -0800

    ACS training config, TinyTim PSF
    From Sebastian

---
+++ paltas/Configs/acs/config_train.py
@@ -0,0 +1,194 @@
+# Configuration for lenses with varied redshift and realistic lens light.
+
+import numpy as np
+from scipy.stats import norm, truncnorm, uniform, lognorm
+from paltas.Substructure.los_dg19 import LOSDG19
+from paltas.Substructure.subhalos_dg19 import SubhalosDG19
+from paltas.MainDeflector.simple_deflectors import PEMDShear
+from paltas.Sources.cosmos import COSMOSExcludeCatalog
+from paltas.Sources.sersic import DoubleSersicData
+from paltas.Sampling import distributions
+from astropy.io import fits
+import pandas as pd
+import paltas
+import os
+
+# Define the numerics kwargs.
+kwargs_numerics = {'supersampling_factor':2,'supersampling_convolution':True}
+# We do not use point_source_supersampling_factor but it must be passed in to
+# surpress a warning.
+kwargs_numerics['point_source_supersampling_factor'] = (
+	kwargs_numerics['supersampling_factor'])
+# This is always the number of pixels for the CCD. If drizzle is used, the
+# final image will be larger.
+numpix = 128
+
+# Define some general image kwargs for the dataset
+mag_cut = 2.0
+
+# Define arguments that will be used multiple times
+output_ab_zeropoint = 25.9
+
+# Define the cosmos path
+root_path = paltas.__path__[0][:-7]
+cosmos_folder = root_path + r'/datasets/cosmos/COSMOS_23.5_training_sample/'
+
+# Set up a callable to grab a tinytim psf from our list. These PSF are
+# generated with a supersampling factor of 2 and include geometric
+# distortions. More details can be found in the parameter files.
+acs_tt_psf_list = [
+	root_path + r'/datasets/hst_psf/acs_tinytim/psf_tt_%d.fits'%(i)
+	for i in range(1,21)]
+
+
+def get_acs_psf():
+	psf_path = np.random.choice(acs_tt_psf_list)
+	hdul = fits.open(psf_path)
+	return hdul[0].data
+
+
+# Set up another callable for the magnitude of the bulge and the fraction
+# of the light in the bulge.
+class MagFBulgeWrapper():
+
+	def __init__(self,mag_bulge_dist,f_bulge_dist):
+		self.mag_bulge_dist = mag_bulge_dist
+		self.f_bulge_dist = f_bulge_dist
+
+	def __call__(self):
+		# Draw the magnitude of the bulge and the fraction of the light
+		# in the bulge.
+		mag_bulge = self.mag_bulge_dist()
+		f_bulge = self.f_bulge_dist()
+
+		# Calculate the total magnitude with that bulge fraction
+		mag = mag_bulge + 2.5*np.log10(f_bulge)
+
+		return mag,f_bulge
+
+
+config_dict = {
+	'subhalo':{
+		'class': SubhalosDG19,
+		'parameters':{
+			'sigma_sub':norm(loc=2e-3,scale=1.1e-3).rvs,
+			'shmf_plaw_index':uniform(loc=-1.92,scale=0.1).rvs,
+			'm_pivot': 1e10,'m_min': 1e7,'m_max': 1e10,
+			'c_0':None,'conc_zeta':None,'conc_beta':None,'conc_m_ref': 1e8,
+			'dex_scatter': None,'k1':0.0, 'k2':0.0
+		}
+	},
+	'los':{
+		'class': LOSDG19,
+		'parameters':{
+			'delta_los':norm(loc=1,scale=0.6).rvs,
+			'm_min':1e7,'m_max':1e10,'z_min':0.01,
+			'dz':0.01,'cone_angle':8.0,'r_min':0.5,'r_max':10.0,
+			'c_0':None,'conc_zeta':None,'conc_beta':None,'conc_m_ref':1e8,
+			'dex_scatter':None,'alpha_dz_factor':5.0
+		}
+	},
+	'main_deflector':{
+		'class': PEMDShear,
+		'parameters':{
+			'M200': 1e13,'z_lens': None,
+			'gamma': truncnorm(-20,np.inf,loc=2.0,scale=0.1).rvs,
+			'theta_E': truncnorm(-1.24/0.26,np.inf,loc=1.25,scale=0.3).rvs,
+			'e1': norm(loc=0.0,scale=0.1).rvs,
+			'e2': norm(loc=0.0,scale=0.1).rvs,
+			'center_x': None,
+			'center_y': None,
+			'gamma1': norm(loc=0.0,scale=0.05).rvs,
+			'gamma2': norm(loc=0.0,scale=0.05).rvs,
+			'ra_0':0.0, 'dec_0':0.0
+		}
+	},
+	'source':{
+		'class': COSMOSExcludeCatalog,
+		'parameters':{
+			'z_source':None,'cosmos_folder':cosmos_folder,
+			'max_z':1.0,'minimum_size_in_pixels':64,'faintest_apparent_mag':20,
+			'smoothing_sigma':0.00,'random_rotation':True,
+			'output_ab_zeropoint':output_ab_zeropoint,
+			'center_x':norm(loc=0.0,scale=0.16).rvs,
+			'center_y':norm(loc=0.0,scale=0.16).rvs,
+			'source_absolute_magnitude':uniform(loc=-24.0,scale=3.0).rvs,
+			'min_flux_radius':10.0,'source_exclusion_list':np.append(
+				pd.read_csv(
+					os.path.join(root_path,'paltas/Sources/bad_galaxies.csv'),
+					names=['catalog_i'])['catalog_i'].to_numpy(),
+				pd.read_csv(
+					os.path.join(root_path,'paltas/Sources/val_galaxies.csv'),
+					names=['catalog_i'])['catalog_i'].to_numpy())}
+	},
+	'lens_light':{
+		'class': DoubleSersicData,
+		'parameters':{
+			'magnitude,f_bulge':MagFBulgeWrapper(
+				mag_bulge_dist=norm(loc=-21.69,scale=0.75).rvs,
+				f_bulge_dist=uniform(loc=0.02,scale=0.95).rvs),
+			'output_ab_zeropoint':output_ab_zeropoint,
+			'n_bulge':norm(loc=4.0,scale=0.2).rvs,
+			'n_disk':norm(loc=1.0,scale=0.1).rvs,
+			'r_disk_bulge':lognorm(scale=np.exp(-1.5),s=2).rvs,
+			'e1_bulge':norm(loc=0.0,scale=0.1).rvs,
+			'e2_bulge':norm(loc=0.0,scale=0.1).rvs,
+			'e1_disk':norm(loc=0.0,scale=0.1).rvs,
+			'e2_disk':norm(loc=0.0,scale=0.1).rvs,
+			'center_x':None,'center_y':None,'z_source':None
+		}
+	},
+	'cosmology':{
+		'parameters':{
+			'cosmology_name': 'planck18'
+		}
+	},
+	'psf':{
+		'parameters':{
+			'psf_type':'PIXEL',
+			'kernel_point_source': get_acs_psf,
+			'point_source_supersampling_factor':2
+		}
+	},
+	'detector':{
+		'parameters':{
+			'pixel_scale':0.050,
+			'ccd_gain':uniform(loc=1.886,scale=0.134).rvs,
+			'read_noise':uniform(loc=5,scale=1).rvs,
+			'magnitude_zero_point':output_ab_zeropoint,
+			'exposure_time':uniform(loc=500,scale=175).rvs,
+			'sky_brightness':uniform(21.13,scale=0.57).rvs,
+			'num_exposures':1,'background_noise':None
+		}
+	},
+	'drizzle':{
+		'parameters':{
+			'supersample_pixel_scale':0.025,'output_pixel_scale':0.050,
+			'wcs_distortion':None,
+			'offset_pattern':[(0,0),(0.5,0),(0.0,0.5),(-0.5,-0.5)],
+			'psf_supersample_factor':2
+		}
+	},
+	'cross_object':{
+		'parameters':{
+			'subhalo:c_0,los:c_0':distributions.Duplicate(
+				dist=uniform(loc=16,scale=2).rvs),
+			'subhalo:conc_zeta,los:conc_zeta':distributions.Duplicate(
+				dist=uniform(loc=-0.3,scale=0.1).rvs),
+			'subhalo:conc_beta,los:conc_beta':distributions.Duplicate(
+				dist=uniform(loc=0.55,scale=0.3).rvs),
+			'subhalo:dex_scatter,los:dex_scatter':distributions.Duplicate(
+				dist=uniform(loc=0.1,scale=0.06).rvs),
+			'main_deflector:z_lens,lens_light:z_source,source:z_source':(
+				distributions.RedshiftsLensLight(z_lens_min=0.06,
+					z_lens_mean=0.24,z_lens_std=0.14,z_source_min=0.3,
+					z_source_mean=0.70,z_source_std=0.24)),
+			'main_deflector:center_x,lens_light:center_x':(
+				distributions.DuplicateScatter(
+					dist=norm(loc=0,scale=0.16).rvs,scatter=0.05)),
+			'main_deflector:center_y,lens_light:center_y':(
+				distributions.DuplicateScatter(
+					dist=norm(loc=0,scale=0.16).rvs,scatter=0.05))
+		}
+	}
+}
