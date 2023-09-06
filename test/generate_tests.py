d9999f96399441fb12fcc635eaf7854f0dd05c73
Phil Holloway <54064261+P-1884@users.noreply.github.com>
2023-09-05 18:37:16 -0700

    Refactoring metadata code into single function

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import unittest
 import sys, glob, copy, os
+import h5py
 from paltas import generate
 import h5py
 try:
@@ -19,6 +20,31 @@ class GenerateTests(unittest.TestCase):
 	def setUp(self):
 		# Set the random seed so we don't run into trouble
 		np.random.seed(20)
+
+	def metadata_check(self,metadata,n_generate):
+		# Function to check that the metadata gives reasonable values
+		self.assertEqual(len(metadata),n_generate)
+		self.assertListEqual(list(
+			metadata['cosmology_parameters_cosmology_name']),
+			['planck18']*n_generate)
+		self.assertListEqual(list(
+			metadata['detector_parameters_pixel_scale']),[0.08]*n_generate)
+		self.assertListEqual(list(metadata['subhalo_parameters_c_0']),
+			[18.0]*n_generate)
+		self.assertListEqual(list(metadata['main_deflector_parameters_z_lens']),
+			[0.5]*n_generate)
+		self.assertListEqual(list(metadata['source_parameters_z_source']),
+			[1.5]*n_generate)
+		self.assertGreater(np.std(metadata['source_parameters_catalog_i']),0)
+		self.assertListEqual(list(metadata['psf_parameters_fwhm']),
+			[0.1]*n_generate)
+		# Check that the subhalo_parameters_sigma_sub are being drawn
+		self.assertGreater(np.std(metadata['los_parameters_delta_los']),
+			0.0)
+		# Check that nothing is getting written under cross_object
+		for key in metadata.keys():
+			self.assertFalse('cross_object' in key)
+			self.assertFalse('source_exclusion_list' in key)
 
 	def test_parse_args(self):
 		# Check that the argument parser works as intended
@@ -55,28 +81,7 @@ class GenerateTests(unittest.TestCase):
 
 		# Make sure the metadata makes sense
 		metadata = pd.read_csv(os.path.join(output_folder,'metadata.csv'))
-		self.assertEqual(len(metadata),n_generate)
-		self.assertListEqual(list(
-			metadata['cosmology_parameters_cosmology_name']),
-			['planck18']*n_generate)
-		self.assertListEqual(list(
-			metadata['detector_parameters_pixel_scale']),[0.08]*n_generate)
-		self.assertListEqual(list(metadata['subhalo_parameters_c_0']),
-			[18.0]*n_generate)
-		self.assertListEqual(list(metadata['main_deflector_parameters_z_lens']),
-			[0.5]*n_generate)
-		self.assertListEqual(list(metadata['source_parameters_z_source']),
-			[1.5]*n_generate)
-		self.assertGreater(np.std(metadata['source_parameters_catalog_i']),0)
-		self.assertListEqual(list(metadata['psf_parameters_fwhm']),
-			[0.1]*n_generate)
-		# Check that the subhalo_parameters_sigma_sub are being drawn
-		self.assertGreater(np.std(metadata['los_parameters_delta_los']),
-			0.0)
-		# Check that nothing is getting written under cross_object
-		for key in metadata.keys():
-			self.assertFalse('cross_object' in key)
-			self.assertFalse('source_exclusion_list' in key)
+		self.metadata_check(metadata,n_generate)
 
 		# Remove the metadata file
 		os.remove(os.path.join(output_folder,'metadata.csv'))
@@ -108,7 +113,7 @@ class GenerateTests(unittest.TestCase):
 
 		image_file_list = glob.glob(os.path.join(output_folder,'image_*.npy'))
 
-		self.assertEqual(len(image_file_list),10)
+		self.assertEqual(len(image_file_list),n_generate)
 
 		# Make sure all of the files are readable and have the correct size
 		# for the config
@@ -119,27 +124,7 @@ class GenerateTests(unittest.TestCase):
 
 		# Make sure the metadata makes sense
 		metadata = pd.read_csv(os.path.join(output_folder,'metadata.csv'))
-		self.assertEqual(len(metadata),10)
-		self.assertListEqual(list(
-			metadata['cosmology_parameters_cosmology_name']),['planck18']*10)
-		self.assertListEqual(list(
-			metadata['detector_parameters_pixel_scale']),[0.08]*10)
-		self.assertListEqual(list(metadata['subhalo_parameters_c_0']),
-			[18.0]*10)
-		self.assertListEqual(list(metadata['main_deflector_parameters_z_lens']),
-			[0.5]*10)
-		self.assertListEqual(list(metadata['source_parameters_z_source']),
-			[1.5]*10)
-		self.assertGreater(np.std(metadata['source_parameters_catalog_i']),0)
-		self.assertListEqual(list(metadata['psf_parameters_fwhm']),
-			[0.1]*10)
-		# Check that the subhalo_parameters_sigma_sub are being drawn
-		self.assertGreater(np.std(metadata['los_parameters_delta_los']),
-			0.0)
-		# Check that nothing is getting written under cross_object
-		for key in metadata.keys():
-			self.assertFalse('cross_object' in key)
-			self.assertFalse('source_exclusion_list' in key)
+		self.metadata_check(metadata,n_generate)
 
 		# Remove the metadata file
 		os.remove(os.path.join(output_folder,'metadata.csv'))
@@ -174,7 +159,6 @@ class GenerateTests(unittest.TestCase):
 		with h5py.File(image_file_list[0],'r') as f0:
 			image_data = f0['data'][()]
 		image_shape = image_data.shape
-		print('image shape',image_data.shape)
 		self.assertEqual(image_shape[0],n_generate)
 
 		# Make sure all of the files are readable and have the correct size
@@ -185,28 +169,7 @@ class GenerateTests(unittest.TestCase):
 
 		# Make sure the metadata makes sense
 		metadata = pd.read_csv(os.path.join(output_folder,'metadata.csv'))
-		self.assertEqual(len(metadata),n_generate)
-		self.assertListEqual(list(
-			metadata['cosmology_parameters_cosmology_name']),
-			['planck18']*n_generate)
-		self.assertListEqual(list(
-			metadata['detector_parameters_pixel_scale']),[0.08]*n_generate)
-		self.assertListEqual(list(metadata['subhalo_parameters_c_0']),
-			[18.0]*n_generate)
-		self.assertListEqual(list(metadata['main_deflector_parameters_z_lens']),
-			[0.5]*n_generate)
-		self.assertListEqual(list(metadata['source_parameters_z_source']),
-			[1.5]*n_generate)
-		self.assertGreater(np.std(metadata['source_parameters_catalog_i']),0)
-		self.assertListEqual(list(metadata['psf_parameters_fwhm']),
-			[0.1]*n_generate)
-		# Check that the subhalo_parameters_sigma_sub are being drawn
-		self.assertGreater(np.std(metadata['los_parameters_delta_los']),
-			0.0)
-		# Check that nothing is getting written under cross_object
-		for key in metadata.keys():
-			self.assertFalse('cross_object' in key)
-			self.assertFalse('source_exclusion_list' in key)
+		self.metadata_check(metadata,n_generate)
 
 		# Remove the metadata file
 		os.remove(os.path.join(output_folder,'metadata.csv'))
@@ -217,7 +180,6 @@ class GenerateTests(unittest.TestCase):
 
 		# Also clean up the test cosmos cache
 		test_cosmo_folder = 'test_data/cosmos/'
-		
 		os.remove(test_cosmo_folder+'paltas_catalog.npy')
 		for i in range(10):
 			os.remove(test_cosmo_folder+'npy_files/img_%d.npy'%(i))
